342d7d89f9b845a51be0a88a0045d71c2d9ba586
nassery322 <109228209+nassery322@users.noreply.github.com>
2023-01-17 13:00:23 +0300

    Update ProductItem.js

--- my-app/src/Components/Products/ProductItem.js
+++ my-app/src/Components/Products/ProductItem.js
@@ -3,33 +3,13 @@ import './ProductItem.css'
 import Button from '../../UI/Button'
 import CartContext from '../../store/cart-context'
 const ProductItem = props =>{
-    const [location, setLocation] = useState(null);
 
-  const addItemHandler = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          setLocation({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-          });
-            
-        },
-          
-        (error) => {
-          console.log(error);
-        },
-        { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-      );
-    } else {
-      console.log("Geolocation is not supported by this browser.");
-    }
-  };
+
     const cartctx = useContext(CartContext)
-//     function addItemHandler(){
-//         cartctx.addItem({id:props.id,name:props.name,
-//         price:props.price})
-//     }
+    function addItemHandler(){
+        cartctx.addItem({id:props.id,name:props.name,
+        price:props.price})
+    }
 
     return <React.Fragment>
 
@@ -47,7 +27,6 @@ const ProductItem = props =>{
 <div className='product-description' style={{'fontSize':'1.2rem'}}>{props.description}</div>
 <div className='section2'>
 <div className='product-price'>${props.price}</div>
-<div>location</div>
 <Button title={'Add to Cart'} onClick={addItemHandler} /> 
 </div>
  </div>
